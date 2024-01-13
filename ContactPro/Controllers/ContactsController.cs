7b97c4e45aaa7a30c50302e797de5247e665e9ec
Joy Dutta <dutta_joy@hotmail.com>
2024-01-13 22:29:57 +0000

    Searching Contacts

--- ContactPro/Controllers/ContactsController.cs
+++ ContactPro/Controllers/ContactsController.cs
@@ -57,12 +57,11 @@ namespace ContactPro.Controllers
             }
             else
             {
-                contacts = appUser.Categories.
-                    FirstOrDefault(c => c.Id == categoryId)
-                   .Contacts
-                   .OrderBy(c => c.LastName)
-                   .ThenBy(c => c.FirstName)
-                   .ToList();
+                contacts = appUser.Categories.FirstOrDefault(c => c.Id == categoryId)
+                    .Contacts
+                    .OrderBy(c => c.LastName)
+                    .ThenBy(c => c.FirstName)
+                    .ToList();
             }
 
             ViewData["CategoryId"] = new SelectList(categories, "Id", "Name", categoryId);
@@ -70,6 +69,40 @@ namespace ContactPro.Controllers
             return View(contacts);
 
         }
+
+        [Authorize]
+        public IActionResult SearchContacts(string searchString)
+        {
+            var contacts = new List<Contact>();
+            string appUserId = _userManager.GetUserId(User);
+
+            // return the userId and its associated contacts and categories
+            AppUser? appUser = _context.Users
+                .Include(c => c.Contacts)
+                .ThenInclude(c => c.Categories)
+                .FirstOrDefault(u => u.Id == appUserId);
+
+            if (String.IsNullOrEmpty(searchString))
+            {
+                contacts = appUser.Contacts
+                                   .OrderBy(c => c.LastName)
+                                   .ThenBy(c => c.FirstName)
+                                   .ToList();
+            }
+            else
+            {
+                contacts = appUser.Contacts.Where(c => c.FullName!.ToLower().Contains(searchString.ToLower()))
+                                   .OrderBy(c => c.LastName)
+                                   .ThenBy(c => c.FirstName)
+                                   .ToList();
+            }
+
+            ViewData["CategoryId"] = new SelectList(appUser.Categories, "Id", "Name", 0);
+
+            return View(nameof(Index), contacts);
+        }
+
+
 
         // GET: Contacts/Details/5
         [Authorize]
